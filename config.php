7fcc8ce3460c29acd7326a8a616b88dde3c39532
is41-sufiyarovrr <rhjirfrfhnjirf>
2022-03-16 13:01:16 +0500

    1.0.2

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-const DB_HOSTNAME = '89.108.83.110';
-const DB_USERNAME = 'is342_eremin';
-const DB_PASSWORD = 'testingmysql';
-const DB_DATABASE = 'is342_eremin';
+const DB_HOSTNAME = 'mysqlis.radiotech.su';
+const DB_USERNAME = 'is341_20_qwe';
+const DB_PASSWORD = 'rhjirfrfhnjirf';
+const DB_DATABASE = 'is341_20_mvc';
 const DB_PORT = '3306'; # Default 3306
